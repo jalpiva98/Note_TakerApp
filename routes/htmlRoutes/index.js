1b9c386565b09f2c9dce5e6fa9002f3a3d3f13d1
Alejandro Piñón <jesuspiva1998@hotmail.com>
2023-08-28 14:59:20 -0600

    added routes and handles for commits

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const router = require('express').Router();
+
+//GET * should return the index.html file.
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+//GET /notes should return the notes.html file.
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+module.exports = router;
